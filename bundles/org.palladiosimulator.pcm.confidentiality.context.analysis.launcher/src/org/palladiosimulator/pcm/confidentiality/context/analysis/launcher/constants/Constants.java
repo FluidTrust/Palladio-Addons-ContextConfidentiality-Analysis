a616594d6a586ddc88a9a688ed75af9941b4b4cd
Maximilian Walter <maximilian.walter@kit.edu>
2022-02-24 16:33:16 +0100

    refactoring for attacker analysis. Removed dependency on variation extension

--- bundles/org.palladiosimulator.pcm.confidentiality.context.analysis.launcher/src/org/palladiosimulator/pcm/confidentiality/context/analysis/launcher/constants/Constants.java
+++ bundles/org.palladiosimulator.pcm.confidentiality.context.analysis.launcher/src/org/palladiosimulator/pcm/confidentiality/context/analysis/launcher/constants/Constants.java
@@ -8,12 +8,15 @@ package org.palladiosimulator.pcm.confidentiality.context.analysis.launcher.cons
  *
  */
 public class Constants {
+
+    private Constants() {
+        // Intentional empty
+    }
+
     public static final String NAME = "Attacker Analysis Modelling Launcher";
 
-    public static final String BUTTON_BROWSE_TEXT = "Browse...";
-    public static final String BUTTON_DIR_BROWSE_TEXT = "Working Directory...";
+
     public static final String ANALYSIS_TYPE_LABEL = "Select Analysis Type";
-    public static final String PROLOG_INTERPRETER_LABEL = "Select PROLOG Interpreter";
     public static final String REPOSITORY_MODEL_LABEL = "Select Repository Model";
     public static final String ALLOCATION_MODEL_LABEL = "Select Allocation Model";
     public static final String MODIFIACTION_MODEL_LABEL = "Select Modifiaction Model";
@@ -21,9 +24,6 @@ public class Constants {
     public static final String DATA_MODEL_LABEL = "Select Data Model";
     public static final String ATTACKER_MODEL_LABEL = "Select Attacker Model";
     public static final String USAGE_MODEL_LABEL = "Select Usage Model";
-    public static final String CONSOLE_ID = "pcm.dataprocessing.context.analysis.launcher.console";
-    public static final String DEFAULT_CONFIG_VALUE = "default";
     public static final String GRAPH_CREATION_LABEL = "Graph Creation";
-    public static final String VARIATION_MODEL_LABEL = "Variation Model";
 
 }
