8a7ec1cd27fa5fc3f2cac49646ac134de512a714
majuwa <maximilian.j.walter@gmail.com>
2022-02-27 17:15:28 +0100

    changed size of assembly context state handler

--- bundles/org.palladiosimulator.pcm.confidentiality.attacker.variation/src/org/palladiosimulator/pcm/confidentiality/attacker/variation/statehandler/pcm/StartComponentStateHandler.java
+++ bundles/org.palladiosimulator.pcm.confidentiality.attacker.variation/src/org/palladiosimulator/pcm/confidentiality/attacker/variation/statehandler/pcm/StartComponentStateHandler.java
@@ -1,39 +1,42 @@
 package org.palladiosimulator.pcm.confidentiality.attacker.variation.statehandler.pcm;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
+import org.palladiosimulator.pcm.confidentiality.attackerSpecification.Attacker;
 
 import UncertaintyVariationModel.VariationPoint;
 import UncertaintyVariationModel.statehandler.GenericStateHandler;
 
 public class StartComponentStateHandler extends GenericStateHandler {
 
-    @Override
-    public List<String> getModelTypes() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private static final String MODEL_TYPE1 = "attacker";
+	private static final String MODEL_TYPE2 = "system	";
 
-    @Override
-    public int getSizeOfDimension(final VariationPoint variationPoint) {
-        var assemblies = variationPoint.getVaryingSubjects();
+	@Override
+	public List<String> getModelTypes() {
+		return Arrays.asList(MODEL_TYPE1, MODEL_TYPE2);
+	}
 
-//        models
-//
-//        for(var element: assemblies) {
-//            var attacker = (Attacker) element;
-//            attacker.getC
-//        }
-        return 0;
-    }
+	@Override
+	public int getSizeOfDimension(final VariationPoint variationPoint) {
+		var assemblies = variationPoint.getVaryingSubjects();
+		if (assemblies.isEmpty())
+			throw new IllegalStateException("Varying subject list is empty");
+		var system = (org.palladiosimulator.pcm.system.System) assemblies.get(0).eContainer();
+		return system.getAssemblyContexts__ComposedStructure().size();
+	}
 
-    @Override
-    public void patchModelWith(final Map<String, List<EObject>> models, final VariationPoint variationPoint,
-            final int variationIdx) {
-        // TODO Auto-generated method stub
-
-    }
+	@Override
+	public void patchModelWith(final Map<String, List<EObject>> models, final VariationPoint variationPoint,
+			final int variationIdx) {
+		final var desc = variationPoint.getVariationDescription();
+		for (final var container : models.get(MODEL_TYPE1)) {
+			final var attacker = (Attacker) container;
+			
+		}
+	}
 
 }
